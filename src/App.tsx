d1dd7306318709796eb228ee7cd39217280416fb
Antti Kekki <antti.kekki@reaktor.com>
2021-10-10 14:31:36 +0300

    Lisää kappaleiden sanoja

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,81 @@ import React, { useState } from "react";
 import _ from "lodash";
 import "./App.css";
 
+enum Kieli {
+  Suomi = "Suomi",
+  Ranska = "Ranska",
+}
+
+enum Kappale {
+  EnClasse = "En classe",
+  OuEstMaGomme = "Où est ma gonne?",
+}
+
 type Sana = { fi: string; fr: string };
 
-const kaikkiSanat: Array<Sana> = [
-  { fi: "poika", fr: "garçon" },
-  { fi: "tyttö", fr: "fille" },
-  { fi: "isä", fr: "papa" },
-  { fi: "äiti", fr: "mère" },
-  { fi: "isoäiti", fr: "grand-mère" },
-  { fi: "kissa", fr: "chat" },
-  { fi: "koira", fr: "chien" },
-  { fi: "hevonen", fr: "cheval" },
-  { fi: "kala", fr: "poisson" },
-];
+const kappaleet: Record<Kappale, Array<Sana>> = {
+  [Kappale.EnClasse]: [
+    { fi: "koulussa", fr: "à l'école" },
+    { fi: "koulu", fr: "une école" },
+    { fi: "koulutarvikkeet", fr: "le matèriel scolaire" },
+    { fi: "luokassa", fr: "dans la trousse" },
+    { fi: "luokassa", fr: "en classe" },
+    { fi: "luokka", fr: "une classe" },
+    { fi: "kertaa", fr: "fois" },
+    { fi: "on yhteensä", fr: "font" },
+    { fi: "helppo", fr: "facile" },
+    { fi: "todella hyvä", fr: "très bien" },
+    { fi: "lapsi", fr: "un enfant" },
+    { fi: "un cours de math", fr: "matematiikan tunti" },
+    { fi: "mutta", fr: "mais" },
+    { fi: "miksi", fr: "pourquoi" },
+    { fi: "pahoillaan", fr: "désolé, désolée" },
+    { fi: "ranskan vihko", fr: "un cahier de français" },
+    { fi: "oho, hups", fr: "oups" },
+    { fi: "antaa lainaksi", fr: "prêter (I)" },
+    { fi: "Lainaatko minulle?", fr: "Tu me prêtes?" },
+    { fi: "tietysti", fr: "bien sûr" },
+    { fi: "ole hyvä (annettaessa)", fr: "voilà" },
+    { fi: "kuinka paljon", fr: "combien de" },
+    { fi: "en tiedä", fr: "je ne sais pas" },
+    { fi: "viisitoista", fr: "quinze" },
+    { fi: "lasketaan ne", fr: "on les compte" },
+  ],
+  [Kappale.OuEstMaGomme]: [
+    { fi: "värit", fr: "les couleurs" },
+    { fi: "Minkä värinen se on?", fr: "C'est de quelle couleur?" },
+    { fi: "eläintaulu", fr: "chanson des animaux" },
+    { fi: "Missä on minun kumini?", fr: "Où est ma gomme?" },
+    { fi: "missä", fr: "où" },
+    { fi: "minun", fr: "mon, ma" },
+    { fi: "anteeksi", fr: "pardon" },
+    { fi: "Ei haittaa", fr: "Ce n'est pas grave." },
+    { fi: "sinun", fr: "ton, ta" },
+    { fi: "Se on totta", fr: "C'est vrai." },
+    {
+      fi: "Minkä värinen sinun kuulakärkikynäsi on?",
+      fr: "Ton stylo est de quelle couleur?",
+    },
+    { fi: "myös", fr: "aussi" },
+    { fi: "nyt", fr: "maintenant" },
+    { fi: "suloinen", fr: "mignon, mignonne" },
+    { fi: "tuolla on", fr: "il y a" },
+    { fi: "paljon", fr: "beaucoup de" },
+    { fi: "ilmapallo, pallo", fr: "un ballon" },
+    { fi: "Mennäänkö sinne?", fr: "On y va?" },
+    { fi: "nopeasti", fr: "vite" },
+  ],
+};
 
 function App() {
+  const [kappale, asetaKappale] = useState<Kappale>(Kappale.EnClasse);
   const [sanat, asetaSanat] = useState<Array<Sana>>([]);
   const [sana, asetaSana] = useState<Sana>(undefined as unknown as Sana);
   const [tiedetyt, asetaTiedetyt] = useState<Array<Sana>>([]);
   const [eiTiedetyt, asetaEiTiedetyt] = useState<Array<Sana>>([]);
   const [peliLoppu, asetaPeliLoppu] = useState<boolean>(true);
   const [näytäSana, asetaNäytäSana] = useState<boolean>(false);
-  const [kysySuomeksi, asetaKysySuomeksi] = useState<boolean>(true);
+  const [kieli, asetaKieli] = useState<Kieli>(Kieli.Suomi);
 
   const siirrySeuraavaanSanaan = () => {
     const jaljellaOlevatSanat = sanat.filter((s) => s.fi !== sana.fi);
@@ -54,7 +107,7 @@ function App() {
   };
 
   const aloitaPeliAlusta = () => {
-    const uudetSanat = _.shuffle(kaikkiSanat);
+    const uudetSanat = _.shuffle(kappaleet[kappale]);
     asetaSanat(uudetSanat);
     asetaSana(uudetSanat[0]);
     asetaTiedetyt([]);
@@ -75,8 +128,14 @@ function App() {
     asetaPeliLoppu(false);
   };
 
-  const vaihdaKieltä = () => {
-    asetaKysySuomeksi(!kysySuomeksi);
+  const vaihdaKieltä = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    asetaKieli(event.target.value as Kieli);
+    aloitaPeliAlusta();
+  };
+
+  const vaihdaKappaletta = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    asetaKappale(event.target.value as Kappale);
+    aloitaPeliAlusta();
   };
 
   return (
@@ -87,12 +146,49 @@ function App() {
         </div>
       </div>
 
+      <div className="row justify-content-md-center">
+        <div className="col col-lg-4">
+          Sanojen kyselykieli:
+          <select className="form-select" onChange={vaihdaKieltä} value={kieli}>
+            <option value={Kieli.Suomi}>Kysy suomeksi</option>
+            <option value={Kieli.Ranska}>Kysy ranskaksi</option>
+          </select>
+        </div>
+      </div>
+
+      <br />
+
+      <div className="row justify-content-md-center">
+        <div className="col col-lg-4">
+          Kappale:
+          <select
+            className="form-select"
+            onChange={vaihdaKappaletta}
+            value={kappale}
+          >
+            <option value={Kappale.EnClasse}>{Kappale.EnClasse}</option>
+            <option value={Kappale.OuEstMaGomme}>{Kappale.OuEstMaGomme}</option>
+          </select>
+        </div>
+      </div>
+
+      <br />
+
+      {!peliLoppu && (
+        <div className="row justify-content-md-center">
+          <div className="col col-lg-4">
+            Sana {tiedetyt.length + eiTiedetyt.length + 1}/
+            {kappaleet[kappale].length}
+          </div>
+        </div>
+      )}
+
       {!peliLoppu && !näytäSana && (
         <div className="row justify-content-md-center">
           <div className="col col-lg-4">
             <div className="card">
               <div className="card-body">
-                {kysySuomeksi ? sana.fi : sana.fr}
+                {kieli === Kieli.Suomi ? sana.fi : sana.fr}
               </div>
             </div>
           </div>
@@ -104,14 +200,14 @@ function App() {
           <div className="col col-lg-2">
             <div className="card">
               <div className="card-body">
-                {kysySuomeksi ? sana.fi : sana.fr}
+                {kieli === Kieli.Suomi ? sana.fi : sana.fr}
               </div>
             </div>
           </div>
           <div className="col col-lg-2">
             <div className="card">
               <div className="card-body">
-                {kysySuomeksi ? sana.fr : sana.fi}
+                {kieli === Kieli.Suomi ? sana.fr : sana.fi}
               </div>
             </div>
           </div>
@@ -154,15 +250,6 @@ function App() {
               Seuraava sana
             </button>
           </div>
-          <div className="col col-lg-2">
-            <button
-              type="button"
-              className="btn btn-warning btn-lg"
-              onClick={() => aloitaPeliAlusta()}
-            >
-              Aloita alusta
-            </button>
-          </div>
         </div>
       )}
 
@@ -174,6 +261,20 @@ function App() {
           <p>Väärin: {eiTiedetyt.length}</p>
         </div>
       </div>
+
+      {!peliLoppu && (
+        <div className="row justify-content-md-center">
+          <div className="col col-lg-4">
+            <button
+              type="button"
+              className="btn btn-warning btn-lg"
+              onClick={() => aloitaPeliAlusta()}
+            >
+              Aloita alusta
+            </button>
+          </div>
+        </div>
+      )}
 
       <br />
 
@@ -199,15 +300,6 @@ function App() {
               </button>
             </div>
           )}
-          <div className="col col-lg-2">
-            <button
-              type="button"
-              className="btn btn-info btn-lg"
-              onClick={() => vaihdaKieltä()}
-            >
-              {kysySuomeksi ? "Kysy ranskaksi" : "Kysy suomeksi"}
-            </button>
-          </div>
         </div>
       )}
     </div>
